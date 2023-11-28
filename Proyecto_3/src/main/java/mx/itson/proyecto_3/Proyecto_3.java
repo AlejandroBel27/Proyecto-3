4394cfecaf44a67cc4f52c0c861665b78f746879
AlejandroBel27 <ruben.saldana216100@potros.itson.edu.mx>
2023-11-27 21:48:35 -0700

    primer commit

---
+++ Proyecto_3/src/main/java/mx/itson/proyecto_3/Proyecto_3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.itson.proyecto_3;
+
+/**
+ *
+ * @author alejandrobel
+ */
+public class Proyecto_3 {
+
+    public static void main(String[] args) {
+        System.out.println("Proyecto BDA 3 final");
+    }
+}
