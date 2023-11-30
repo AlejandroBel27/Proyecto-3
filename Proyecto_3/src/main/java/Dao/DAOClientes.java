01a9965f0fc2b46c4b032f9551f1b31d2fd2552c
leyv4a <leyv4a@leyv4a>
2023-11-30 11:29:05 -0700

    DAOClientes, DAOHabitaciones se implementaron con sus respectivos metodos abstractos

--- Proyecto_3/src/main/java/Dao/DAOClientes.java
+++ Proyecto_3/src/main/java/Dao/DAOClientes.java
@@ -4,10 +4,63 @@
  */
 package Dao;
 
+import Exceptions.DAOException;
+import Objetos.Clientes;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import java.util.ArrayList;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
 /**
  *
  * @author alejandrobel
  */
-public class DAOClientes {
-    
+public class DAOClientes extends BaseDao<Clientes> {
+
+    @Override
+    public ArrayList consultar() throws DAOException {
+        MongoCollection<Clientes> collection = this.getColeccion();
+        return collection.find().into(new ArrayList<>());
+    }
+
+    @Override
+    public void insertar(Clientes entidad) throws DAOException {
+        MongoCollection<Clientes> collection = this.getColeccion();
+        collection.insertOne(entidad);
+    }
+
+    @Override
+    public void actualizar(Clientes entidad) throws DAOException {
+        MongoCollection<Clientes> collection = getColeccion();
+        collection.updateOne(Filters.eq("_id", entidad.getId()),
+                new Document("$set", new Document()
+                        .append("ID_cliente", entidad.getID_cliente())
+                        .append("nombre", entidad.getNombre())
+                        .append("direccion", entidad.getDireccion())
+                        .append("telefono", entidad.getTelefono())
+                )
+        );
+    }
+
+    @Override
+    public Clientes consultarPorId(ObjectId id) throws DAOException {
+        MongoCollection<Clientes> collection = getColeccion();
+        return collection.find(Filters.eq("_id", id)).first();
+    }
+
+    @Override
+    public void eliminar(ObjectId id) throws DAOException {
+        MongoCollection<Clientes> collection = getColeccion();
+        Clientes cliente = collection.findOneAndDelete(Filters.eq("_id", id));
+        if (cliente == null) {
+            System.out.println("No se encontró el cliente con ID: " + id);
+        }
+    }
+
+    private MongoCollection<Clientes> getColeccion() throws DAOException {
+        MongoCollection<Clientes> Clientes = this.generarConexion().getCollection("clientes", Clientes.class);
+        return Clientes;
+    }
+
 }
--- Proyecto_3/src/main/java/Dao/DAOHabitaciones.java
+++ Proyecto_3/src/main/java/Dao/DAOHabitaciones.java
@@ -4,10 +4,64 @@
  */
 package Dao;
 
+import Exceptions.DAOException;
+import Objetos.Habitaciones;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import java.util.ArrayList;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
 /**
  *
  * @author alejandrobel
  */
-public class DAOHabitaciones {
-    
+public class DAOHabitaciones extends BaseDao<Habitaciones> {
+
+    @Override
+    public ArrayList<Habitaciones> consultar() throws DAOException {
+        MongoCollection<Habitaciones> collection = this.getColeccion();
+        return collection.find().into(new ArrayList<>());
+    }
+
+    @Override
+    public void insertar(Habitaciones entidad) throws DAOException {
+        MongoCollection<Habitaciones> collection = getColeccion();
+        collection.insertOne(entidad);
+    }
+
+    @Override
+    public void actualizar(Habitaciones entidad) throws DAOException {
+        MongoCollection<Habitaciones> collection = getColeccion();
+        collection.updateOne(Filters.eq("_id", entidad.getId()),
+                new Document("$set", new Document()
+                        .append("ID_habitacion", entidad.getID_habitacion())
+                        .append("numero", entidad.getNumero())
+                        .append("tipo", entidad.getTipo())
+                        .append("planta", entidad.getPlanta())
+                        .append("ID_hotel", entidad.getID_hotel())
+                )
+        );
+    }
+
+    @Override
+    public Habitaciones consultarPorId(ObjectId id) throws DAOException {
+          MongoCollection<Habitaciones> collection = getColeccion();
+        return collection.find(Filters.eq("_id", id)).first();
+    }
+
+    @Override
+    public void eliminar(ObjectId id) throws DAOException {
+          MongoCollection<Habitaciones> collection = getColeccion();
+        Habitaciones habitacion = collection.findOneAndDelete(Filters.eq("_id", id));
+        if (habitacion == null) {
+            System.out.println("No se encontró la habitación con ID: " + id);
+        }
+    }
+
+    private MongoCollection<Habitaciones> getColeccion() throws DAOException {
+        MongoCollection<Habitaciones> Habitaciones = this.generarConexion().getCollection("habitaciones", Habitaciones.class);
+        return Habitaciones;
+    }
+
 }
