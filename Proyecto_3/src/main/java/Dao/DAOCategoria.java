fb8652fed626411bea40c6a160a21463ae0f625f
AlejandroBel27 <ruben.saldana216100@potros.itson.edu.mx>
2023-11-29 23:27:21 -0700

    DAOCategoria falta implementar metodos abstractos

--- Proyecto_3/src/main/java/Dao/DAOCategoria.java
+++ Proyecto_3/src/main/java/Dao/DAOCategoria.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Dao;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import Exceptions.DAOException;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import Objetos.Categoria;
+import java.util.ArrayList;
 
 /**
  *
  * @author alejandrobel
  */
-public class DAOCategoria {
+public class DAOCategoria extends BaseDao<Categoria>{
+
+    @Override
+    public ArrayList<Categoria> consultar() throws DAOException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void insertar(Categoria entidad) throws DAOException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void actualizar(Categoria entidad) throws DAOException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Categoria consultarPorId(ObjectId id) throws DAOException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void eliminar(ObjectId id) throws DAOException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
     
 }
