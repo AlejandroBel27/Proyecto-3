03589fab3152ece2fd89647aa6736e47cddde7d8
leyv4a <leyv4a@leyv4a>
2023-11-30 11:09:11 -0700

    DAOCategoria se implementaron los metodos abstractos

--- Proyecto_3/src/main/java/Dao/DAOCategoria.java
+++ Proyecto_3/src/main/java/Dao/DAOCategoria.java
@@ -1,4 +1,3 @@
-
 package Dao;
 
 import com.mongodb.client.MongoCollection;
@@ -13,32 +12,50 @@ import java.util.ArrayList;
  *
  * @author alejandrobel
  */
-public class DAOCategoria extends BaseDao<Categoria>{
+public class DAOCategoria extends BaseDao<Categoria> {
 
     @Override
     public ArrayList<Categoria> consultar() throws DAOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        MongoCollection<Categoria> collection = this.getColeccion();
+        return collection.find().into(new ArrayList<>());
     }
 
     @Override
     public void insertar(Categoria entidad) throws DAOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+         MongoCollection<Categoria> collection = this.getColeccion();
+        collection.insertOne(entidad);
     }
 
     @Override
     public void actualizar(Categoria entidad) throws DAOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+          MongoCollection<Categoria> collection = getColeccion();
+        collection.updateOne(Filters.eq("_id", entidad.getId()),
+                new Document("$set", new Document()
+                        .append("ID_categoria", entidad.getID_categoria())
+                        .append("nombre", entidad.getNombre())
+                        .append("tipo_IVA", entidad.getTipo_IVA())
+                        .append("estrellas", entidad.getEstrellas())
+                )
+        );
     }
 
     @Override
     public Categoria consultarPorId(ObjectId id) throws DAOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+         MongoCollection<Categoria> collection = getColeccion();
+        return collection.find(Filters.eq("_id", id)).first();
     }
 
     @Override
     public void eliminar(ObjectId id) throws DAOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        MongoCollection<Categoria> collection = getColeccion();
+        Categoria categoria = collection.findOneAndDelete(Filters.eq("_id", id));
+        if (categoria == null) {
+            System.out.println("No se encontró la categoría con ID: " + id);
+        }
     }
-    
-    
+
+    protected MongoCollection<Categoria> getColeccion() throws DAOException{
+         MongoCollection<Categoria> Categoria = this.generarConexion().getCollection("categoria", Categoria.class);
+         return Categoria;
+    }
 }
